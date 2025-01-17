a590903716e89628cd6d97bcefabcc707316ee41
Al Cerdan Lico <alcerdan@yahoo.com>
2025-01-17 10:09:57 -0800

    creating database

---
+++ server/migrations/versions/1b55b0558908_create_db.py
@@ -0,0 +1,24 @@
+"""create DB
+
+Revision ID: 1b55b0558908
+Revises: 
+Create Date: 2025-01-17 09:28:12.020250
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1b55b0558908'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- server/models.py
+++ server/models.py
@@ -1,6 +1,62 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
-from config import db
+from config import db, datetime
 
 # Models go here!
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    password = db.Column(db.String)
+    full_name = db.Column(db.String)
+    profile_pic = db.Column(db.String)
+    address = db.Column(db.String)
+    city = db.Column(db.String)
+    state = db.Column(db.String)
+
+    #Add relationships
+    purchases = db.Relationship(
+        'Purchases', back_populates='user', cascase='all, delete-orphan')
+    
+    # Add serialization rules
+    serialize_rules = ('purchases.user',)
+
+class Event(db.Model, SerializerMixin):
+    __tablename__ = 'events'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    venue = db.Column(db.String)
+    city = db.Column(db.String)
+    state = db.Column(db.String)
+    date = db.Column(db.Datetime)
+    price = db.Column(db.Float)
+    
+    #Add relationships
+    purchases = db.Relationship(
+        'Purchases', back_populates='event', cascase='all, delete-orphan')
+    
+    # Add serialization rules
+    serialize_rules = ('purchases.event',)
+
+class Purchase(db.Model, SerializerMixin):
+    __tablename__ = 'purchases'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    number_tickets = db.Column(db.Integer)
+
+    #Add relationships
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
+
+    user = db.Relationship('User', back_populates='purchases')
+    event = db.Relationship('Event', back_populates='purchases')
+
+    # Add serialization rules
+    serialize_rules = ('-user.purchases', '-event.purchases',)
+
