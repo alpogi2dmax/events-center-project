199aa424eacfdf6a48441dcd61ffd9d7af2437a0
Al Cerdan Lico <alcerdan@yahoo.com>
2025-01-19 08:56:35 -0800

    added sort in mylist

--- client/src/components/MyList.js
+++ client/src/components/MyList.js
@@ -3,9 +3,21 @@ import MyCard from './MyCard'
 
 function MyList({purchases, onDeletePurchase}) {
 
+    if (purchases.length === 0) {
+        return (
+            <p>There are no events in your list for now. Please purchase tickets.</p>
+        )
+    }
+
+    const sortedPurchases = purchases.sort((a, b) => {
+        const dateA = new Date(a.event.date);
+        const dateB = new Date(b.event.date);
+        return dateA.getTime() - dateB.getTime()
+    });
+
     return (
         <div className='list'>
-            {purchases.map(purchase => (
+            {sortedPurchases.map(purchase => (
                 <MyCard key={purchase.id} purchase={purchase} onDeletePurchase={onDeletePurchase}/>
             ))}
         </div>
