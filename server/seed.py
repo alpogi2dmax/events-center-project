d2e243d6565340c590869280579bb9cf2ff20c33
Al Cerdan Lico <alcerdan@yahoo.com>
2025-01-19 23:58:45 -0800

    added in seed

--- server/seed.py
+++ server/seed.py
@@ -29,12 +29,22 @@ if __name__ == '__main__':
         users = []
         users.append(User(username = 'lebronjames', password = 'p@ssw0rd', full_name = 'Lebron James', profile_pic = './images/lebronjames.png', address = '9955 W Beverly Grove Dr', city = 'Beverly Hills', state = 'CA'))
         users.append(User(username = 'conanobrien', password = 'p@ssw0rd', full_name = 'Conan OBrien', profile_pic = 'images/conanobrien.png', address = '1253 Amalfi Dr', city = 'Los Angeles', state = 'CA'))
+        users.append(User(username = 'tomcruise', password = 'p@ssw0rd', full_name = 'Tom Cruise', profile_pic = 'images/tomcruise.png', address = '1111 Calle Vista Dr', city = 'Beverly Hills', state = 'CA'))
+        users.append(User(username = 'billgates', password = 'p@ssw0rd', full_name = 'Bill Gates', profile_pic = 'images/billgates.png', address = '1835 73rd Ave NE', city = 'Medina', state = 'WA'))
+
 
         print('Seeding events...')
         events = []
         events.append(Event(name = 'Mary J Blige', image = './images/maryjblige.png', venue = 'Intuit Dome', city = 'Inglewood', state = 'CA', date = datetime.strptime('03/01/2025', '%m/%d/%Y'), price = 123))
         events.append(Event(name = 'J Balvin', image = './images/jbalvin.png', venue = 'Kia Forum', city = 'Inglewood', state = 'CA', date = datetime.strptime('05/09/2025', '%m/%d/%Y'), price = 123))
         events.append(Event(name = 'Heart', image = './images/heart.png', venue = 'Golden 1 Center', city = 'Sacramento', state = 'CA', date = datetime.strptime('03/04/2025', '%m/%d/%Y'), price = 42))
+        events.append(Event(name = 'AC/DC', image = './images/acdc.png', venue = 'Rose Bowl', city = 'Pasadena', state = 'CA', date = datetime.strptime('04/18/2025', '%m/%d/%Y'), price = 129))
+        events.append(Event(name = 'Dua Lipa', image = './images/dualipa.png', venue = 'United Center', city = 'Chicago', state = 'IL', date = datetime.strptime('09/05/2025', '%m/%d/%Y'), price = 207))
+        events.append(Event(name = 'Kendrick Lamar', image = './images/kendricklamar.png', venue = 'NRG Stadium', city = 'Houston', state = 'TX', date = datetime.strptime('04/23/2025', '%m/%d/%Y'), price = 182))
+        events.append(Event(name = 'Metallica', image = './images/metallica.png', venue = 'Raymond James Stadium', city = 'Tampa', state = 'FL', date = datetime.strptime('06/06/2025', '%m/%d/%Y'), price = 52))
+        events.append(Event(name = 'Post Malone', image = './images/postmalone.png', venue = 'Allegiant Stadium', city = 'Las Vegas', state = 'NV', date = datetime.strptime('05/03/2025', '%m/%d/%Y'), price = 91))
+        events.append(Event(name = 'Tyler the Creator', image = './images/tylerthecreator.png', venue = 'Crypto.com Arena', city = 'Los Angeles', state = 'CA', date = datetime.strptime('02/15/2025', '%m/%d/%Y'), price = 182))
+        events.append(Event(name = 'Charli XCX', image = './images/charlixcx.png', venue = 'Barclays Center', city = 'Brooklyn', state = 'NY', date = datetime.strptime('05/04/2025', '%m/%d/%Y'), price = 207))
 
         print('Seeding purchases...')
         purchases = []
